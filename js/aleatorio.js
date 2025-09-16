92229b32a2d87456f9f902cb546b1893c0e38327
oda777 <murilo.augusto.barbosa@escola.pr.gov.br>
2025-09-16 10:55:55 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["marlon", "jonssons", "jeffersom", "nelha", "valdileia", "antonela"];
 
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,105 @@
-
+export const perguntas = [
+    {
+        enunciado: "Você prefere doce ou salgado?",  //a pergunta
+        alternativas: [
+            {
+                texto: "doce", //primeira alternativa
+                afirmacao: [
+                    "você tem bom gosto", //comentário para a resposta
+                    "afirmação 2"        //segundo comentário
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "salgado",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 1,
+            }
+        ]
+    },
+    {
+        enunciado: "você prefere prime video ou netflix",
+        alternativas: [
+            {
+                texto: "prime video",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "netflix",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "você prefere batman ou superman",
+        alternativas: [
+            {
+                texto: "batman",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "superman",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "você prefere 2 reais ou presente misterioso",
+        alternativas: [
+            {
+                texto: "2 reais",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "presente misterioso",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "você prefere baggy ou skinny",
+        alternativas: [
+            {
+                texto: "baggy",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "skinny",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+            }
+        ]
+    }
+]
